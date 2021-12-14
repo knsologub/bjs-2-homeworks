db11eedfeb18e1c64eea74609e4b4af93fb801df
Konstantin Sologub <konstantin.sologub@gmail.com>
2021-12-14 23:54:30 +0300

    Первое задание, вторая лекия "Функции"

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,11 +1,39 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg;
+  let quantity;
+
+  arr = [1, 2, 3, -100, 10];
+
+  //Search for minimum and maximum values
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    } else if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  //Calculate the sum
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  quantity = arr.length;
+
+  let average = sum / quantity;
+
+  avg = average.toFixed(2);
 
   // Ваш код
 
-  return { min: min, max: max, avg: avg };
+  return { min: min, max: max, avg: +avg };
 }
+
+console.log(getArrayParams());
 
 // Задание 2
 function worker(arr) {
@@ -21,7 +49,7 @@ function makeWork(arrOfArr, func) {
 
   // Ваш кода
   // for ...
-  
+
   return max;
 }
 
