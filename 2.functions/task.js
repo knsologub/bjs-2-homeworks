b9faa91fe6b593c8866278464355e70676bfeb63
Konstantin Sologub <konstantin.sologub@gmail.com>
2021-12-18 16:58:26 +0300

    Добавлен код 2-ой и 3-ей задач

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,10 +1,9 @@
-// Задание 1
+//                                        Задание 1
 function getArrayParams(arr) {
   let min = Infinity;
   let max = -Infinity;
   let sum = 0;
   let avg;
-  let quantity;
 
   //Search for minimum and maximum values
   for (let i = 0; i < arr.length; i++) {
@@ -20,39 +19,61 @@ function getArrayParams(arr) {
   }
 
   //Calculate the sum
-
-  quantity = arr.length;
-
-  let average = sum / quantity;
+  let average = sum / arr.length;
 
   avg = Number(average.toFixed(2));
-
-  // Ваш код
 
   return { min: min, max: max, avg: avg };
 }
 
-console.log(getArrayParams());
+//console.log(getArrayParams([50, 500]));
 
-// Задание 2
+//                                          Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
+worker();
 
-  // Ваш кода
-  // for ...
+function makeWork(arrOfArr, func) {
+  let max = -Infinity;
+
+  for (let i = 0; i < arrOfArr.length; i++) {
+    console.log(func(arrOfArr[i]));
+
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i]);
+    }
+  }
+
+  //console.log(max);
 
   return max;
 }
 
-// Задание 3
+makeWork();
+
+//                                             Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return Math.abs(max - min);
 }
+worker2();
+
+//console.log(worker2([5, 10]))
